23c89c6c73942a9ed93848be37a415fe52e7a866
Bill Doody <wdoody@yahoo.com>
2018-05-15 04:17:20 -0600

    jwt token auth

---
+++ config/routes.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var characterController = require('../controller/characters');
+var placeController = require('../controller/places');
+
+//CHARACTER ROUTES
+router.get('/api/characters', characterController.characterIndex);
+router.get('/api/heroes', characterController.characterHeroIndex);
+router.get('/api/villains', characterController.characterVillainIndex);
+
+router.post('/api/characters', characterController.characterHeroCreate);
+router.put('/api/characters', characterController.characterUpdate);
+
+
+
+
+//PLACES ROUTES
+router.get('/api/places/', placeController.placeIndex);
+router.get('/api/places/:place_id', placeController.placeIndexID);
+router.post('/api/places', placeController.placeCreate);
+router.put("/api/places", placeController.placeUpdate);
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,41 +1,6 @@
-require('dotenv').config()
+var app = require('./app');
+var port = process.env.PORT || 3000;
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex')
-
-const signin = require('./controllers/signin');
-const register = require('./controllers/register');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
-
-// initialize database
-const db = knex({
-  client: 'pg',
-  connection: {
-    connectionString: 'postgres://jjdgeszssptiml:84977e9a62755e351393fab43ca0531a5ce19ee74703beb1b7329af7286066d8@ec2-50-19-224-165.compute-1.amazonaws.com:5432/d3qeeker20imt',
-    ssl: true,
-  }
+var server = app.listen(port, function () {
+  console.log('Express server listening on port ' + port);
 });
-
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
-
-app.get('/', (req, res) => {
-  if (req.body) res.send(db.users);
-});
-
-app.post('/signin', signin.handleSignin(db, bcrypt));
-
-app.post('/register', register.handleRegister(db, bcrypt));
-
-app.get('/profile/:id', profile.handleGetProfile(db));
-
-app.put('/image', image.handleSubmitImage(db));
-app.post('/imageurl', image.handleApiCall);
-
-const PORT = (process.env.PORT || 3000);
-app.listen(PORT, () => console.log(`app is running on port ${PORT}`));
